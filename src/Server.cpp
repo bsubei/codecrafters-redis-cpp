ef12050762f82ef5ad4783968e8aed39271f76ab
Basheer Subei <6508762+bsubei@users.noreply.github.com>
2024-06-13 12:25:22 +0000

    slight cpp refactoring

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,3 +1,4 @@
+#include <optional>
 #include <iostream>
 #include <cstdlib>
 #include <string>
@@ -8,20 +9,14 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-int main(int argc, char **argv)
+std::optional<int> create_server_socket()
 {
-  // Flush after every std::cout / std::cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  std::cout << "Logs from your program will appear here!\n";
-
+  // Create the socket.
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0)
   {
     std::cerr << "Failed to create server socket\n";
-    return 1;
+    return std::nullopt;
   }
 
   // Since the tester restarts your program quite often, setting SO_REUSEADDR
@@ -33,35 +28,67 @@ int main(int argc, char **argv)
     return 1;
   }
 
+  // Bind the socket to the desired address/port.
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(6379);
-
   if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0)
   {
     std::cerr << "Failed to bind to port 6379\n";
-    return 1;
+    return std::nullopt;
   }
 
+  // Listen on that socket.
   int connection_backlog = 5;
   if (listen(server_fd, connection_backlog) != 0)
   {
     std::cerr << "listen failed\n";
-    return 1;
+    return std::nullopt;
   }
 
+  return server_fd;
+}
+
+int await_client_connection(int server_fd)
+{
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
 
   std::cout << "Waiting for a client to connect...\n";
 
   int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
-  char pong[] = "+PONG\r\n";
-  send(client_fd, pong, sizeof(pong) - 1, 0);
-  std::cout << "Client connected\n";
+  std::cout << "Client " << client_fd << " connected!";
+  return client_fd;
+}
 
-  close(server_fd);
+void send_to_client(int client_fd, const std::string &message)
+{
+  send(client_fd, message.c_str(), message.size(), 0);
+}
+
+int main(int argc, char **argv)
+{
+  // Flush after every std::cout / std::cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+
+  // You can use print statements as follows for debugging, they'll be visible when running tests.
+  std::cout << "Logs from your program will appear here!\n";
+
+  std::optional<int> server_fd = create_server_socket();
+  if (!server_fd)
+  {
+    return 1;
+  }
+
+  int client_fd = await_client_connection(*server_fd);
+  // TODO read client's message.
+
+  std::string message{"+PONG\r\n"};
+  send_to_client(client_fd, message);
+
+  close(*server_fd);
 
   return 0;
 }
