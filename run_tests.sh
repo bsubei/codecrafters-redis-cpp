6fd6f319c28f6a4a3d74c06ed16edafacb9e565e
Basheer Subei <6508762+bsubei@users.noreply.github.com>
2024-07-06 23:28:50 +0000

    rename test and cmake test target

--- run_tests.sh
+++ run_tests.sh
@@ -14,7 +14,7 @@ trap cleanup EXIT
 # Configure the project
 cmake -B build -S .
 # Build only the test target
-cmake --build ./build --target parser_test
+cmake --build ./build --target redis_tests
 # Switch to build directory but save dir so we can cleanup when done.
 pushd build
 # Run the tests
