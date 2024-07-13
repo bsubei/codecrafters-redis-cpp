2fc336e004d455b27f8d3cc5346777743e9b2ba3
Basheer Subei <6508762+bsubei@users.noreply.github.com>
2024-07-13 14:16:55 +0000

    skeleton code for storage and RDB sections

--- src/storage.cpp
+++ src/storage.cpp
@@ -2,16 +2,29 @@
 #include "storage.hpp"
 
 // System includes.
+#include <exception>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
 
 // Our library's header includes.
 #include "config.hpp"
 
+namespace {
+// void read_rdb_header(std::istringstream &is) { is.get(); }
+} // namespace
+
 Cache read_cache_from_rdb(const Config &config) {
   if (config.dbfilename && config.dir) {
-    std::cout << "Reading RDB from file: " << *config.dir << "/"
-              << *config.dbfilename << std::endl;
-    // TODO Read the file contents.
+    const auto filepath = std::filesystem::path(*config.dir) /
+                          std::filesystem::path(*config.dbfilename);
+    std::cout << "Reading RDB from file: " << filepath << std::endl;
+
+    std::ifstream file_contents(filepath, std::ios::binary);
+    if (!file_contents) {
+      std::cerr << "Failed to read file contents at: " << filepath << std::endl;
+      std::terminate();
+    }
 
     // TODO Parse the file contents (only the key-value cache data for now).
   }
--- src/storage.hpp
+++ src/storage.hpp
@@ -1,8 +1,54 @@
 #pragma once
 
+// System includes.
+#include <array>
+#include <cstdint>
+#include <optional>
+#include <unordered_map>
+#include <vector>
+
 // Our library's header includes.
 #include "cache.hpp"
 
 struct Config;
 
 Cache read_cache_from_rdb(const Config &config);
+
+// RDB file format, see https://rdb.fnordig.de/file_format.html for details.
+// Op codes
+constexpr uint8_t RDB_EOF = 0xFF;
+constexpr uint8_t RDB_DB_SELECTOR = 0xFE;
+constexpr uint8_t RDB_EXPIRE_TIME_S = 0xFD;
+constexpr uint8_t RDB_EXPIRE_TIME_MS = 0xFC;
+constexpr uint8_t RDB_AUX = 0xFA; // Auxiliary fields (AKA metadata fields).
+// TODO we only support version 7 exactly.
+constexpr auto RDB_MAGIC = "REDIS0007";
+
+enum class NumBits {
+  ARCHITECTURE_32_BITS,
+  ARCHITECTURE_64_BITS,
+};
+
+struct Header {
+  std::uint8_t version = 0;
+};
+struct Metadata {
+  std::optional<std::uint64_t> creation_time{};
+  std::optional<std::uint64_t> used_memory{}; // TODO units?
+  std::optional<std::uint8_t> redis_version{};
+  std::optional<NumBits> redis_num_bits{};
+};
+struct DatabaseSection {
+  std::unordered_map<Cache::KeyT, Cache::EntryT> data{};
+};
+struct EndOfFile {
+  std::array<std::uint8_t, 8> crc64{};
+};
+
+// An RDB file consists of these sections.
+struct RDB {
+  Header header{};
+  Metadata metadata{};
+  std::vector<DatabaseSection> database_sections{};
+  EndOfFile eof{};
+};
